b195bd20b67a5234967708911f8e6fc0cb622751
Seow Alex <seowalex@hotmail.com>
2021-02-26 01:39:50 +0800

    Update AppDatabaseTests

--- PeggleTests/AppDatabaseTests.swift
+++ PeggleTests/AppDatabaseTests.swift
@@ -1,3 +1,5 @@
+// swiftlint:disable type_body_length file_length
+
 import XCTest
 import GRDB
 @testable import Peggle
@@ -19,7 +21,7 @@ class AppDatabaseTests: XCTestCase {
             let columns = try db.columns(in: "level")
             let columnNames = Set(columns.map { $0.name })
 
-            XCTAssertEqual(columnNames, ["id", "name"])
+            XCTAssertEqual(columnNames, ["id", "name", "isProtected"])
         }
     }
 
@@ -33,7 +35,17 @@ class AppDatabaseTests: XCTestCase {
         }
     }
 
-    func testSaveLevel_insertsValidPegs_success() throws {
+    func testDatabaseSchema_blockSchema() throws {
+        try dbWriter.read { db in
+            try XCTAssertTrue(db.tableExists("block"))
+            let columns = try db.columns(in: "block")
+            let columnNames = Set(columns.map { $0.name })
+
+            XCTAssertEqual(columnNames, ["id", "levelId", "position", "rotation", "size"])
+        }
+    }
+
+    func testSaveLevel_insertsValidElements_success() throws {
         var level = LevelRecord(name: "Asteroid Blues")
 
         let size = CGSize(width: 40, height: 40)
@@ -42,8 +54,13 @@ class AppDatabaseTests: XCTestCase {
             PegRecord(position: CGPoint(x: 40, y: 0), size: size, color: .blue),
             PegRecord(position: CGPoint(x: 0, y: 40), size: size, color: .orange)
         ]
+        var blocks = [
+            BlockRecord(position: CGPoint(x: 100, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 140, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 100, y: 140), size: size)
+        ]
 
-        try appDatabase.saveLevel(&level, pegs: &pegs)
+        try appDatabase.saveLevel(&level, pegs: &pegs, blocks: &blocks)
 
         try dbWriter.read { db in
             try XCTAssertTrue(level.exists(db))
@@ -51,24 +68,33 @@ class AppDatabaseTests: XCTestCase {
             for peg in pegs {
                 try XCTAssertTrue(peg.exists(db))
             }
+
+            for block in blocks {
+                try XCTAssertTrue(block.exists(db))
+            }
         }
     }
 
-    func testSaveLevel_insertsInvalidPegs_throwsError() throws {
+    func testSaveLevel_insertsInvalidElements_throwsError() throws {
         var level = LevelRecord(name: "Asteroid Blues")
 
         let size = CGSize(width: 40, height: 40)
-        // Pegs that are colliding
+        // Elements that are colliding
         var pegs = [
             PegRecord(position: .zero, size: size, color: .blue),
-            PegRecord(position: CGPoint(x: 20, y: 0), size: size, color: .blue),
-            PegRecord(position: CGPoint(x: 0, y: 20), size: size, color: .orange)
+            PegRecord(position: CGPoint(x: 40, y: 0), size: size, color: .blue),
+            PegRecord(position: CGPoint(x: 0, y: 40), size: size, color: .orange)
+        ]
+        var blocks = [
+            BlockRecord(position: CGPoint(x: 0, y: 0), size: size),
+            BlockRecord(position: CGPoint(x: 40, y: 0), size: size),
+            BlockRecord(position: CGPoint(x: 0, y: 40), size: size)
         ]
 
-        try XCTAssertThrowsError(appDatabase.saveLevel(&level, pegs: &pegs))
+        try XCTAssertThrowsError(appDatabase.saveLevel(&level, pegs: &pegs, blocks: &blocks))
     }
 
-    func testSaveLevel_insertsNewLevelOldPegs_throwsError() throws {
+    func testSaveLevel_insertsNewLevelWithSameName_overridesOldLevel() throws {
         var oldLevel = LevelRecord(name: "Asteroid Blues")
 
         let size = CGSize(width: 40, height: 40)
@@ -76,6 +102,11 @@ class AppDatabaseTests: XCTestCase {
             PegRecord(position: .zero, size: size, color: .blue),
             PegRecord(position: CGPoint(x: 40, y: 0), size: size, color: .blue),
             PegRecord(position: CGPoint(x: 0, y: 40), size: size, color: .orange)
+        ]
+        var blocks = [
+            BlockRecord(position: CGPoint(x: 100, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 140, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 100, y: 140), size: size)
         ]
 
         try dbWriter.write { db in
@@ -85,15 +116,64 @@ class AppDatabaseTests: XCTestCase {
                 pegs[index].levelId = oldLevel.id
                 try pegs[index].insert(db)
             }
+
+            for index in 0..<blocks.count {
+                blocks[index].levelId = oldLevel.id
+                try blocks[index].insert(db)
+            }
         }
 
-        var newLevel = LevelRecord(name: "Stray Dog Strut")
+        XCTAssertEqual(try dbWriter.read(oldLevel.pegs.fetchCount), 3)
+        XCTAssertEqual(try dbWriter.read(oldLevel.blocks.fetchCount), 3)
 
-        // Trying to save a level with another level's pegs should throw an error
-        try XCTAssertThrowsError(appDatabase.saveLevel(&newLevel, pegs: &pegs))
+        var newLevel = LevelRecord(name: "Asteroid Blues")
+        var newPegs: [PegRecord] = []
+        var newBlocks: [BlockRecord] = []
+
+        try appDatabase.saveLevel(&newLevel, pegs: &newPegs, blocks: &newBlocks)
+
+        XCTAssertEqual(oldLevel.id, newLevel.id)
+        XCTAssertEqual(try dbWriter.read(oldLevel.pegs.fetchCount), 0)
+        XCTAssertEqual(try dbWriter.read(oldLevel.blocks.fetchCount), 0)
     }
 
-    func testSaveLevel_updatesAddPegs_success() throws {
+    func testSaveLevel_insertsNewLevelWithSameNameAsProtectedLevel_throwsError() throws {
+        var oldLevel = LevelRecord(name: "Asteroid Blues", isProtected: true)
+
+        let size = CGSize(width: 40, height: 40)
+        var pegs = [
+            PegRecord(position: .zero, size: size, color: .blue),
+            PegRecord(position: CGPoint(x: 40, y: 0), size: size, color: .blue),
+            PegRecord(position: CGPoint(x: 0, y: 40), size: size, color: .orange)
+        ]
+        var blocks = [
+            BlockRecord(position: CGPoint(x: 100, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 140, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 100, y: 140), size: size)
+        ]
+
+        try dbWriter.write { db in
+            try oldLevel.insert(db)
+
+            for index in 0..<pegs.count {
+                pegs[index].levelId = oldLevel.id
+                try pegs[index].insert(db)
+            }
+
+            for index in 0..<blocks.count {
+                blocks[index].levelId = oldLevel.id
+                try blocks[index].insert(db)
+            }
+        }
+
+        var newLevel = LevelRecord(name: "Asteroid Blues")
+        var newPegs: [PegRecord] = []
+        var newBlocks: [BlockRecord] = []
+
+        XCTAssertThrowsError(try appDatabase.saveLevel(&newLevel, pegs: &newPegs, blocks: &newBlocks))
+    }
+
+    func testSaveLevel_updatesAddElements_success() throws {
         var level = LevelRecord(name: "Asteroid Blues")
 
         let size = CGSize(width: 40, height: 40)
@@ -101,6 +181,11 @@ class AppDatabaseTests: XCTestCase {
             PegRecord(position: .zero, size: size, color: .blue),
             PegRecord(position: CGPoint(x: 40, y: 0), size: size, color: .blue),
             PegRecord(position: CGPoint(x: 0, y: 40), size: size, color: .orange)
+        ]
+        var blocks = [
+            BlockRecord(position: CGPoint(x: 100, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 140, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 100, y: 140), size: size)
         ]
 
         try dbWriter.write { db in
@@ -110,22 +195,34 @@ class AppDatabaseTests: XCTestCase {
                 pegs[index].levelId = level.id
                 try pegs[index].insert(db)
             }
+
+            for index in 0..<blocks.count {
+                blocks[index].levelId = level.id
+                try blocks[index].insert(db)
+            }
         }
 
-        // Add some new pegs
+        // Add some new elements
         pegs.append(contentsOf: [
             PegRecord(position: CGPoint(x: 80, y: 0), size: size, color: .blue),
             PegRecord(position: CGPoint(x: 0, y: 80), size: size, color: .orange)
         ])
 
-        try appDatabase.saveLevel(&level, pegs: &pegs)
+        blocks.append(contentsOf: [
+            BlockRecord(position: CGPoint(x: 180, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 100, y: 180), size: size)
+        ])
+
+        try appDatabase.saveLevel(&level, pegs: &pegs, blocks: &blocks)
 
         let fetchedPegs = try dbWriter.read(level.pegs.fetchAll)
+        let fetchedBlocks = try dbWriter.read(level.blocks.fetchAll)
 
         XCTAssertEqual(fetchedPegs, pegs)
+        XCTAssertEqual(fetchedBlocks, blocks)
     }
 
-    func testSaveLevel_updatesEditPegs_success() throws {
+    func testSaveLevel_updatesDeleteElements_success() throws {
         var level = LevelRecord(name: "Asteroid Blues")
 
         let size = CGSize(width: 40, height: 40)
@@ -133,6 +230,11 @@ class AppDatabaseTests: XCTestCase {
             PegRecord(position: .zero, size: size, color: .blue),
             PegRecord(position: CGPoint(x: 40, y: 0), size: size, color: .blue),
             PegRecord(position: CGPoint(x: 0, y: 40), size: size, color: .orange)
+        ]
+        var blocks = [
+            BlockRecord(position: CGPoint(x: 100, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 140, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 100, y: 140), size: size)
         ]
 
         try dbWriter.write { db in
@@ -142,48 +244,24 @@ class AppDatabaseTests: XCTestCase {
                 pegs[index].levelId = level.id
                 try pegs[index].insert(db)
             }
-        }
 
-        // Edit some pegs
-        for index in 0..<pegs.count {
-            pegs[index].position.x += 40
-            pegs[index].position.y += 40
-        }
-
-        try appDatabase.saveLevel(&level, pegs: &pegs)
-
-        let fetchedPegs = try dbWriter.read(level.pegs.fetchAll)
-
-        XCTAssertEqual(fetchedPegs, pegs)
-    }
-
-    func testSaveLevel_updatesDeletePegs_success() throws {
-        var level = LevelRecord(name: "Asteroid Blues")
-
-        let size = CGSize(width: 40, height: 40)
-        var pegs = [
-            PegRecord(position: .zero, size: size, color: .blue),
-            PegRecord(position: CGPoint(x: 40, y: 0), size: size, color: .blue),
-            PegRecord(position: CGPoint(x: 0, y: 40), size: size, color: .orange)
-        ]
-
-        try dbWriter.write { db in
-            try level.insert(db)
-
-            for index in 0..<pegs.count {
-                pegs[index].levelId = level.id
-                try pegs[index].insert(db)
+            for index in 0..<blocks.count {
+                blocks[index].levelId = level.id
+                try blocks[index].insert(db)
             }
         }
 
-        // Delete some pegs
+        // Delete some elements
         pegs.removeLast()
+        blocks.removeLast()
 
-        try appDatabase.saveLevel(&level, pegs: &pegs)
+        try appDatabase.saveLevel(&level, pegs: &pegs, blocks: &blocks)
 
         let fetchedPegs = try dbWriter.read(level.pegs.fetchAll)
+        let fetchedBlocks = try dbWriter.read(level.blocks.fetchAll)
 
         XCTAssertEqual(fetchedPegs, pegs)
+        XCTAssertEqual(fetchedBlocks, blocks)
     }
 
     func testDeleteLevels() throws {
@@ -198,6 +276,11 @@ class AppDatabaseTests: XCTestCase {
             PegRecord(position: CGPoint(x: 40, y: 0), size: size, color: .blue),
             PegRecord(position: CGPoint(x: 0, y: 40), size: size, color: .orange)
         ]
+        var blocks = [
+            BlockRecord(position: CGPoint(x: 100, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 140, y: 100), size: size),
+            BlockRecord(position: CGPoint(x: 100, y: 140), size: size)
+        ]
 
         try dbWriter.write { db in
             try level1.insert(db)
@@ -208,6 +291,11 @@ class AppDatabaseTests: XCTestCase {
             for index in 0..<pegs.count {
                 pegs[index].levelId = level1.id
                 try pegs[index].insert(db)
+            }
+
+            for index in 0..<blocks.count {
+                blocks[index].levelId = level1.id
+                try blocks[index].insert(db)
             }
         }
 
@@ -222,9 +310,13 @@ class AppDatabaseTests: XCTestCase {
             try XCTAssertFalse(level1.exists(db))
             try XCTAssertFalse(level3.exists(db))
 
-            // Deleting levels should also delete their associated pegs
+            // Deleting levels should also delete their associated elements
             for peg in pegs {
                 try XCTAssertFalse(peg.exists(db))
+            }
+
+            for block in blocks {
+                try XCTAssertFalse(block.exists(db))
             }
         }
 
@@ -311,5 +403,29 @@ class AppDatabaseTests: XCTestCase {
         let fetchedPegs = try appDatabase.fetchPegs(level)
 
         XCTAssertEqual(fetchedPegs, pegs)
+    }
+
+    func testFetchBlocks() throws {
+        var level = LevelRecord(name: "Asteroid Blues")
+
+        let size = CGSize(width: 40, height: 40)
+        var blocks = [
+            BlockRecord(position: .zero, size: size),
+            BlockRecord(position: CGPoint(x: 40, y: 0), size: size),
+            BlockRecord(position: CGPoint(x: 0, y: 40), size: size)
+        ]
+
+        try dbWriter.write { db in
+            try level.insert(db)
+
+            for index in 0..<blocks.count {
+                blocks[index].levelId = level.id
+                try blocks[index].insert(db)
+            }
+        }
+
+        let fetchedBlocks = try appDatabase.fetchBlocks(level)
+
+        XCTAssertEqual(fetchedBlocks, blocks)
     }
 }
